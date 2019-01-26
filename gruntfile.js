02716438caa22e268fc8d2416adda3907806c499
Vladimir <dik2043@mail.ru>
2019-01-26 12:28:04 +0400

    вроде нормальная сборка

--- gruntfile.js
+++ gruntfile.js
@@ -1,37 +1,16 @@
 "use strict";
 
 module.exports = function(grunt) {
-    // grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-browser-sync");
     grunt.loadNpmTasks("grunt-contrib-watch");
-    // grunt.loadNpmTasks("grunt-postcss");
-
     grunt.initConfig({
-        // less: {
-        //     style: {
-        //         files: {
-        //             "css/style.css": "less/style.less"
-        //         }
-        //     }
-        // },
-
-        // postcss: {
-        //     style: {
-        //         options: {
-        //             processors: [
-        //                 require("autoprefixer")()
-        //             ]
-        //         },
-        //         src: "css/*css"
-        //     }
-        // },
-
+        
         browserSync: {
             server: {
                 bsFiles: {
                     src: [
                         "*.html",
-                        "css/*css"
+                        "js/*js"
                     ]
                 },
                 options: {
